f1617ee71953a2c47888f7bd238a56d009f0caef
Max Bernstein <tekknolagi@gmail.com>
2014-01-22 07:32:08 -0800

    fixed posts & blog

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,22 +1,19 @@
+/*
+ * Code written by Maxwell Bernstein and originally used for
+ * bernsteinbear.com. This work is licensed under a
+ * Creative Commons Attribution-NonCommercial 3.0 Unported License.
+ * For attribution, please clearly mention my name and my website, Bernstein Bear.
+ * License: http://creativecommons.org/licenses/by-nc/3.0/deed.en_US
+ * Bernstein Bear: http://bernsteinbear.com
+ */
+
 window.posts = [];
 
 var PostList = function PostListF () {
     var postList = $("#post-list");
     var postSingle = $("#post-single");
-    var listUL = postList.find("ul");
     postSingle.hide();
     postList.show();
-
-    $.each(window.posts, function (ind, post) {
-	var listEl = $("<li/>");
-
-	$("<a/>", {
-	    href: post.link,
-	    html: post.title
-	}).appendTo(listEl);
-
-	listEl.appendTo(listUL);
-    });
 };
 
 var SinglePost = function SinglePostF (link) {
@@ -48,7 +45,19 @@ $(document).ready(function () {
 	dataType: "text",
 	success: function(data) {
 	    window.posts = JSON.parse(data);
-	    var names = []
+
+	    $.each(window.posts, function (ind, post) {
+		var postList = $("#post-list");
+		var listUL = postList.find("ul");
+		var listEl = $("<li/>");
+
+		$("<a/>", {
+		    href: post.link,
+		    html: post.title
+		}).appendTo(listEl);
+
+		listEl.appendTo(listUL);
+	    });
 
 	    routie({
 		'': PostList,
