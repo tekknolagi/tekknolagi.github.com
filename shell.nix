4a4dd0aaea18cdb4569ae0830d141393603d6a1f
Max Bernstein <max@bernsteinbear.com>
2023-12-12 02:03:30 -0500

    Pin nixpkgs version

--- shell.nix
+++ shell.nix
@@ -1,9 +1,7 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/5d43a8764fa6b933eb01b8bb0f4bd253226bf261.tar.gz") {} }:
   pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
        jekyll
        rubyPackages.jekyll-feed
-       # rubyPackages.webrick
-       # rubyPackages.jekyll-redirect-from
        ];
 }
