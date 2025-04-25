548011820c9acaf23c8af05402940251716d74ed
Max Bernstein <max@bernsteinbear.com>
2025-04-25 01:08:43 -0400

    Fix set -u

--- new.sh
+++ new.sh
@@ -2,7 +2,8 @@
 set -eu
 
 title="$1"
-slug="$2" || "$(echo "$title"| tr '[:upper:]' '[:lower:]' | sed -e 's/ /-/g' -e 's/[^a-zA-Z-]//g')"
+default_slug="$(echo "$title"| tr '[:upper:]' '[:lower:]' | sed -e 's/ /-/g' -e 's/[^a-zA-Z-]//g')"
+slug="${2:-$default_slug}"
 date="$(date +"%Y-%m-%d")"
 root="$(git rev-parse --show-toplevel)"
 filename="$root/_posts/$date-$slug.md"
@@ -10,6 +11,6 @@ relpath="$(realpath --relative-to="${PWD}" "$filename")"
 # Alternatively, if on an older machine or macOS (note intentionally swapped argument order)
 #   relpath="$(python3 -c "import os.path; print(os.path.relpath('$filename', '${PWD}'))")"
 if ! [ -f "$relpath" ]; then
-  echo "---\ntitle: \"$title\"\n---\n\n" > "$relpath"
+  echo "---\ntitle: \"$title\"\nlayout: post\n---\n\n" > "$relpath"
 fi
 echo "$relpath"
