efbe6886cabaeace1b8c978a04eae89ab8ddaaed
Max Bernstein <max@bernsteinbear.com>
2025-06-30 21:26:27 -0400

    .

--- new.sh
+++ new.sh
@@ -7,9 +7,9 @@ slug="${2:-$default_slug}"
 date="$(date +"%Y-%m-%d")"
 root="$(git rev-parse --show-toplevel)"
 filename="$root/_posts/$date-$slug.md"
-relpath="$(realpath --relative-to="${PWD}" "$filename")"
-# Alternatively, if on an older machine or macOS (note intentionally swapped argument order)
-#   relpath="$(python3 -c "import os.path; print(os.path.relpath('$filename', '${PWD}'))")"
+relpath="$(python3 -c "import os.path; print(os.path.relpath('$filename', '${PWD}'))")"
+# Alternatively, but does not work on an older machine or macOS (note intentionally swapped argument order)
+# relpath="$(realpath --relative-to="${PWD}" "$filename")"
 if ! [ -f "$relpath" ]; then
   echo "---\ntitle: \"$title\"\nlayout: post\n---\n\n" > "$relpath"
 fi
